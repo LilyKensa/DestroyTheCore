1d1a9147882ef90c7e09fc1283a7c854a246d801
Itz-Yoyo <35717yoyo@gmail.com>
2025-10-31 17:25:42 +0800

    Sekiro Main Update New features: - Hello World typo fixes

--- src/main/java/dev/huey/destroyTheCore/roles/SekiroRole.java
+++ src/main/java/dev/huey/destroyTheCore/roles/SekiroRole.java
@@ -6,7 +6,7 @@ import dev.huey.destroyTheCore.managers.RolesManager;
 public class SekiroRole extends Role {
 
     public static void main(String[] args){
-        System.out.println("Hellow World");
+        System.out.println("Hello World");
     }
     public SekiroRole(RolesManager.RoleKey id) {
         super(id);
