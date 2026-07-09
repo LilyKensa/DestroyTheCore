7704b991fb7808b9179e2867f074fdf7442a28e5
Itz-Yoyo <35717yoyo@gmail.com>
2026-07-09 09:47:17 +0800

    ⌚ fix ores cooldown

--- src/main/java/dev/huey/destroyTheCore/Constants.java
+++ src/main/java/dev/huey/destroyTheCore/Constants.java
@@ -25,7 +25,7 @@ public class Constants {
         Material.COAL_ORE,
         Material.COAL,
         NamedTextColor.GRAY,
-        5,
+        10,
         3,
         9
       )
@@ -36,7 +36,7 @@ public class Constants {
         Material.IRON_ORE,
         Material.RAW_IRON,
         NamedTextColor.WHITE,
-        7,
+        10,
         0,
         0
       )
@@ -47,7 +47,7 @@ public class Constants {
         Material.GOLD_ORE,
         Material.RAW_GOLD,
         NamedTextColor.YELLOW,
-        8,
+        10,
         0,
         0
       )
@@ -58,7 +58,7 @@ public class Constants {
         Material.REDSTONE_ORE,
         Material.REDSTONE,
         NamedTextColor.RED,
-        5,
+        10,
         1,
         5
       )
@@ -80,7 +80,7 @@ public class Constants {
         Material.EMERALD_ORE,
         Material.EMERALD,
         NamedTextColor.GREEN,
-        13,
+        15,
         2,
         6
       )
@@ -91,7 +91,7 @@ public class Constants {
         Material.DIAMOND_ORE,
         Material.DIAMOND,
         NamedTextColor.AQUA,
-        90,
+        60,
         4,
         8
       )
